a378b6471877c7ee65548657d36844aaecb53813
Janelle K Hirano <jaahnelle@gmail.com>
2019-06-04 22:15:36 -1000

    added firstFactorial function, letterCapitalize function

--- exercises.js
+++ exercises.js
@@ -1,11 +1,62 @@
 // Function -  FirstFactorial(num)
 // The function will take the num parameter being passed and return the factorial of it (ie. if num =3, return(3 * 2 * 1)).  For test cases, the range will be between 1 and 18.
 
+// FIRST ATTEMPT:
+// function firstFactorial(num) {
+//     for (var i = 1; i < 19; i++) {
+//         // console.log(firstFactorial[i]);
+//         // console.log([i]);
+//         if (num === 0) {
+//             return 1;
+//         }
+//         // console.log(num)
+//         return num * firstFactorial(num - 1);
+//     }
+//     console.log(firstFactorial[i]);
+// }
+
+function firstFactorial(num) {
+    if (num === 1) {
+        return 1;
+    } else {
+        return num * firstFactorial(num - 1);
+    }
+}
+console.log(firstFactorial(18));
+
 // Function - SimpleAdding(num)
 // The function will take the num parameter and add up all the numbers from 1 to num.  For test cases, the parameter num will be any number from 1 to 1000.
 
+// FIRST ATTEMPT:
+// function simpleAdding(num){
+//     if (num )
+// for (var i = 0; i<simpleAdding.length; i++){
+//     console.log(simpleAdding(3);
+// }
+// }
+
+
 // Function - LetterCapitalize(str)
 // The function will take the str parameter being passed and capitalize the first letter of each word.  Words will be separated by only one space.
+
+// FIRST ATTEMPT:
+// var ahhh = 'this is fricken hard';
+
+// function letterCapitalize(str){
+    // return str.upperCase();
+    // return str.charAt(0).toUpperCase() + str.slice(1) FIRST 
+// }
+
+var ahhh = 'this is fricken hard';
+
+function letterCapitalize(str) {
+    var capIt = str.toLowerCase().split(' '); //DECLARE A VARIABLE AND ASSIGN IT A VALUE THAT CHANGES THE STRING INTO LOWER CASE LETTERS AND SPLITTING THEM INTO SUBSTRINGS 
+    for (var i = 0; i < capIt.length; i++) { //LOOP THROUGH SUBSTRINGS
+        capIt[i] = capIt[i].charAt(0).toUpperCase() + capIt[i].substring(1); // DEFINE LOOP - RETURN CHARACTOR AT 0 POSITION (1ST LETTER) OF EACH SUBSTRING AND RETURN VAR CAPIT (LOWERCASE) FROM 1 POSITION (2ND LETTER)
+    }
+    return capIt.join(' '); // JOIN SUBSTRINGS INTO STRING
+}
+console.log(letterCapitalize(ahhh));
 
 // Function rangeRover(arr)
 // The function will take an array of two numbers and return the sum of those two numbers AND all numbers between them.  The lowest number will not always come first.  For example rangeRover([1, 4]) should return 10, i.e.(1 + 2 + 3 + 4), rangeRover([4, 1]) should also return 10.
